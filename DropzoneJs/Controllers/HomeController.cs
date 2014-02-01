7cc4127ce8210097b2e15341508cc647eda4be2d
Sampath-Lokuge <sugeesampathlokuge@gmail.com>
2014-02-02 00:05:09 +0530

    DropzoneJSApp

---
+++ DropzoneJs/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropzoneJs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        /// <summary>
+        /// to Save DropzoneJs Uploaded Files
+        /// </summary>
+        public ActionResult SaveDropzoneJsUploadedFiles()
+        {
+            foreach (string fileName in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[fileName];
+                //You can Save the file content here
+            }
+
+            return Json(new { Message = string.Empty });
+        }
+    }
+}
